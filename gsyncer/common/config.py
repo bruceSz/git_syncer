2d0dd3a29407c087c67a0cb1a2a8ac870a4b6812
bruceSz <zsyuyizhang@gmail.com>
2014-05-26 22:58:09 +0800

    rewrite config half done

--- gsyncer/common/config.py
+++ gsyncer/common/config.py
@@ -4,15 +4,32 @@ import sys
 CONF = None
 
 config_name="gsyncer.conf"
-possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
-                                                os.pardir,
-                                                os.pardir))
+#possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
+#                                                os.pardir,
+#                                                os.pardir))
 print possible_topdir
 if not os.path.exists(os.path.join(possible_topdir,"etc",config_name)):
     print >> stderr, "config file not found!!!"
     sys.exit(1)
 
 config_path= os.path.join(possible_topdir,"etc")
+
+class Config(object):
+    def __init__(self,config_path,config_file):
+        cf=ConfigParser.ConfigParser()
+        cf.read(config_file)
+
+        scs = cf.sections()
+        for sc in scs:
+            ops = cf.options(sc)
+            for op in ops:
+                if op == 'repos':
+                    repos = cf.get(sc,op)
+                    repos = repos.split(',')
+                    self.__dict__[op]=repos
+                else:
+                    self.__dict__[op] = cf.get(sc,op)
+
 
 def _init():
     global CONF
@@ -22,36 +39,12 @@ def _init():
         CONF = _do_init()
         return CONF
 
-
-
 def _do_init():
 
-    global config_name
-    global possible_topdir 
-    global config_path
-
-    class Conf:
-        pass
-    
-    conf = Conf()
-
-    cf=ConfigParser.ConfigParser()
-
-    cf.read(os.path.join(config_path,config_name))
-
-    scs = cf.sections()
-    for sc in scs:
-        ops = cf.options(sc)
-        for op in ops:
-            if op == 'repos':
-                repos = cf.get(sc,op)
-                repos = repos.split(',')
-                conf.__dict__[op]=repos
-            else:
-                conf.__dict__[op] = cf.get(sc,op)
-
-    return conf
-
+    #global config_name
+    #global possible_topdir 
+    #global config_path
+    pass
 
 def test_config_parser():
     cf = ConfigParser.ConfigParser()
@@ -62,5 +55,5 @@ def test_config_parser():
         print op,cf.get("default",op)
 
 
-CONF = _init()
+CONF = Config()
 
--- gsyncer/tests/test_config.py
+++ gsyncer/tests/test_config.py
@@ -8,6 +8,7 @@ possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
 if os.path.exists(os.path.join(possible_topdir,"gsyncer", "__init__.py")):
         sys.path.insert(0, possible_topdir)
 
+print possible_topdir
 
 from gsyncer.common import  config
 conf = config.CONF
